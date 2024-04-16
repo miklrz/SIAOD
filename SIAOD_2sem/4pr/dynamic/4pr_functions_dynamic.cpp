3856f28081a4a7bd6d5e32870aec62a8f2f08d69
hxastur <arz.mikhail@gmail.com>
2024-04-16 12:33:43 +0300

    4

--- SIAOD_2sem/4pr/dynamic/4pr_functions_dynamic.cpp
+++ SIAOD_2sem/4pr/dynamic/4pr_functions_dynamic.cpp
@@ -3,8 +3,6 @@
 using namespace std;
 
 void inputMatrix(typeX* matrix) {
-	cout << "n: "; cin >> matrix->n;
-	cout << "m: "; cin >> matrix->m;
 	for (int i = 0; i < matrix->n; ++i) {
 		for (int j = 0; j < matrix->m; ++j) {
 			cout << i << " " << j << " элемент матрицы: ";
